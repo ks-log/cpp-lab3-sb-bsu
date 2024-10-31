60978b95e8e78b1ad8f0d8fd390f110d6617bee4
ks-log <ksenialogunova25@gmail.com>
2024-10-31 12:01:30 +0300

    Update 3a-t4.cpp

--- 3a-t4.cpp
+++ 3a-t4.cpp
@@ -1,17 +1,36 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
+
 int main()
 {
 int k, i = 1, sum_k = 0, number;
-cout << " Enter the amount of integers to sum = "; cin >> k;
+
+cout << "Enter the amount of integers to sum = ";
+cin >> k;
+
+if (cin.fail())
+    {
+    cin.clear();
+    cin.ignore(); 
+    cout << "Invalid input." << endl << "Enter an integer for the amount of integers to sum: ";
+    cin >> k;
+    }
 if (k == 0) { cout << "The total sum of 0 integers is: 0" << endl; }
 else {
-do {
-cout << "Enter integer nr. " << i << ": ";
-cin >> number;
-sum_k += number; i++;
-} while (i <= k);
-cout << " The total sum of " << k << " integers is: " << sum_k;
-}
+      do {
+           cout << "Enter integer nr. " << i << ": ";
+           cin >> number;
+           if (cin.fail())
+           {
+            cin.clear();
+            cin.ignore(); 
+            cout << "Invalid input." << endl << "Please enter an integer: ";
+            cin >> number;
+           }   
+           sum_k += number; i++;
+         }
+while (i <= k);
+cout << "The total sum of " << k << " integers is: " << sum_k << endl;
+     }
 return 0;
 }
